6010707c7f57ce9a247e91878943369ada589048
Kihun Song <kiihuns22@gmail.com>
2025-11-09 23:54:20 +0900

    fork() sets the same priority for the child

--- proc.c
+++ proc.c
@@ -205,6 +205,7 @@ int fork(void)
   np->sz = curproc->sz;
   np->parent = curproc;
   *np->tf = *curproc->tf;
+  np->priority = curproc->priority; //  child shares the same priority as the parent
 
   // Clear %eax so that fork returns 0 in the child.
   np->tf->eax = 0;
