f8b19fb7b82fe64a2bcbfa0530c8a97b661fc1c3
Kihun Song <kiihuns22@gmail.com>
2025-11-10 22:22:37 +0900

    better test.c

--- test.c
+++ test.c
@@ -2,14 +2,16 @@
 #include "stat.h"
 #include "user.h"
 
-#define PROCESS_NUM 5
-#define PROCESS_DUMMY_LOOP 500000000
+// starvation test routine
+#define PROCESS_NUM 4
+#define PROCESS_DUMMY_LOOP 5000000
 
 int main(int argc, char *argv[])
 {
     int pid = -2;
 
-    for (int i = 0; i < PROCESS_NUM; i++)
+    // high priority proc
+    for (int i = PROCESS_NUM - 1; i > 0; i--)
     {
         pid = fork();
         if (pid < 0)
@@ -27,22 +29,99 @@ int main(int argc, char *argv[])
             {
                 dummy += (j * j) % 997;
                 dummy ^= (j << 3);
+
+                if (j == PROCESS_DUMMY_LOOP - 1) // endless loop
+                {
+                    printf(1, "Proc %d priority %d running\n", getpid(), get_proc_priority(getpid()));
+                    j = 0;
+                }
             }
 
-            printf(1, "Proc %d priority %d finished (res=%d)\n", getpid(), i + 1, (int)dummy);
+            printf(1, "Proc %d priority %d finished (res=%d)\n", getpid(), get_proc_priority(getpid()), (int)dummy);
             exit();
         }
         else // parent
         {
-            set_proc_priority(pid, i + 1);
+            set_proc_priority(pid, 2);
         }
     }
+
+    // low priority proc
+    pid = fork();
+    if (pid < 0)
     {
-        for (int i = 0; i < PROCESS_NUM; i++)
+        printf(1, "process fork failed!\n");
+        exit();
+    }
+
+    if (pid == 0) // child
+    {
+        // set_proc_priority(getpid(), i + 1);
+        volatile long long dummy = 0;
+
+        for (int j = 0; j < PROCESS_DUMMY_LOOP; j++) // dummy load
         {
-            wait();
+            dummy += (j * j) % 997;
+            dummy ^= (j << 3);
         }
+
+        printf(1, "Proc %d priority %d finished (res=%d)\n", getpid(), get_proc_priority(getpid()), (int)dummy);
+        exit();
+    }
+    else // parent
+    {
+        set_proc_priority(pid, 8);
+    }
+
+    for (int i = 0; i < PROCESS_NUM; i++)
+    {
+        wait();
     }
 
     exit();
 }
+
+// // test routine
+// #define PROCESS_NUM 5
+// #define PROCESS_DUMMY_LOOP 1000000
+
+// int main(int argc, char *argv[])
+// {
+//     int pid = -2;
+
+//     for (int i = PROCESS_NUM - 1; i >= 0; i--)
+//     {
+//         pid = fork();
+//         if (pid < 0)
+//         {
+//             printf(1, "process fork failed!\n");
+//             exit();
+//         }
+
+//         if (pid == 0) // child
+//         {
+//             // set_proc_priority(getpid(), i + 1);
+//             volatile long long dummy = 0;
+
+//             for (int j = 0; j < PROCESS_DUMMY_LOOP; j++) // dummy load
+//             {
+//                 dummy += (j * j) % 997;
+//                 dummy ^= (j << 3);
+//             }
+
+//             printf(1, "Proc %d priority %d finished (res=%d)\n", getpid(), get_proc_priority(getpid()), (int)dummy);
+//             exit();
+//         }
+//         else // parent
+//         {
+//             set_proc_priority(pid, (i + 1) % 10);
+//         }
+//     }
+
+//     for (int i = 0; i < PROCESS_NUM; i++)
+//     {
+//         wait();
+//     }
+
+//     exit();
+// }
